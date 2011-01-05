d6058ffedf122ee6222cb66665e79d211e034525
Boris Terzic <boris@terzic.be>
2011-01-05 21:38:36 +0100

    initial commit

---
+++ pinboard-save-bookmark/selection.js
@@ -0,0 +1,7 @@
+// From http://stackoverflow.com/questions/3767429/button-in-popup-that-get-selected-text-chrome-extension
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+    if (request.method == "getSelection")
+      sendResponse({data: window.getSelection().toString()});
+    else
+      sendResponse({}); // snub them.
+});
