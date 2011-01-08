ace15229a60e10868cf0dfd578179518d608834f
Boris Terzic <boris@terzic.be>
2011-01-08 22:33:17 +0100

    Horrible type, a dot after a closing else branch statement caused the plugin to fail

--- pinboard-save-bookmark/selection.js
+++ pinboard-save-bookmark/selection.js
@@ -3,5 +3,5 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
     if (request.method == "getSelection")
       sendResponse({data: window.getSelection().toString()});
     else
-      sendResponse({});.
+      sendResponse({});
 });
